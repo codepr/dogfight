123b69811ca43c24e283d732b91641f4376b21f9
Andrea Baldan <a.g.baldan@gmail.com>
2025-01-05 01:17:11 +0000

    Add simple vector 2D module

---
+++ server/lib/dogfight/game/vec2.ex
@@ -0,0 +1,24 @@
+defmodule Dogfight.Game.Vec2 do
+  @moduledoc """
+  Utility module to perform operations on Vector 2D
+  """
+
+  @type t :: %__MODULE__{
+          x: integer(),
+          y: integer()
+        }
+
+  defstruct [:x, :y]
+
+  def add(v1, v2) do
+    %__MODULE__{x: v1.x + v2.x, y: v1.y + v2.y}
+  end
+
+  def add_x(v, x) do
+    %__MODULE__{x: v.x + x, y: v.y}
+  end
+
+  def add_y(v, y) do
+    %__MODULE__{x: v.x, y: v.y + y}
+  end
+end
